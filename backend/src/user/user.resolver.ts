509573df18e555aa6f7c3210321ea62ee69a0752
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-06 09:15:59 +0900

    :sparkles: user機能作成

---
+++ backend/src/user/user.resolver.ts
@@ -0,0 +1,23 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { CreateUserInput } from './dto/createUser.input';
+import { User as UserModel } from './models/user.model';
+import { User } from '@prisma/client';
+import { GetUserArgs } from './dto/getUser.args';
+
+@Resolver()
+export class UserResolver {
+  constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => UserModel)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return await this.userService.createUser(createUserInput);
+  }
+
+  @Query(() => UserModel, { nullable: true })
+  async getUser(@Args() getUserArgs: GetUserArgs): Promise<User> {
+    return await this.userService.getUser(getUserArgs.email);
+  }
+}
