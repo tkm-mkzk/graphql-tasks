1e0ba372b07962a8f291e95ec8eede3833173bda
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-07 09:39:21 +0900

    :sparkles: [frontend] 環境構築とルーティング

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { GuestRoute, PrivateRoute } from './AuthRoute'
+import Main from './components/Main'
+import NotFound from './components/NotFound'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<GuestRoute children={<SignIn />} />} />
+        <Route path="/signup" element={<GuestRoute children={<SignUp />} />} />
+        <Route path="/" element={<PrivateRoute children={<Main />} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
