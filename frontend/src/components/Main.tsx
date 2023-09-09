5522a694ab629379b607ff745635b13b81d494a3
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-09 15:50:15 +0900

    :sparkles: [frontend] メインページ実装

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,10 +1,36 @@
+import jwtDecode from 'jwt-decode'
+import TaskTable from './TaskTable'
 import Header from './header'
+import { Payload } from '../types/payload'
+import { useQuery } from '@apollo/client'
+import { GET_TASKS } from '../queries/taskQueries'
+import { Task } from '../types/task'
+import Loading from './loading'
+import { Stack, Typography } from '@mui/material'
+import AddTask from './AddTask'
 
 const Main = () => {
+  const token = localStorage.getItem('token')
+  const decordedToken = jwtDecode<Payload>(token!)
+  const userId = decordedToken.sub
+
+  const { loading, data, error } = useQuery<{ getTasks: Task[] }>(GET_TASKS, {
+    variables: { userId },
+  })
+
   return (
     <>
       <Header />
-      <div>Main</div>
+      <Stack spacing={4} direction="column" m={8} alignItems="center">
+        {loading && <Loading />}
+        {error && <Typography color="red">エラーが発生しました</Typography>}
+        {!loading && !error && (
+          <>
+            <AddTask userId={userId} />
+            <TaskTable tasks={data?.getTasks} userId={userId} />
+          </>
+        )}
+      </Stack>
     </>
   )
 }
